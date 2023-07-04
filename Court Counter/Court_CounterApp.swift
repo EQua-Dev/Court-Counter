e0de609f4aea65973db2284f3c5825176ea0df06
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2023-07-04 04:30:37 +0100

    Initial Commit

---
+++ Court Counter/Court_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Court_CounterApp.swift
+//  Court Counter
+//
+//  Created by Richard Uzor on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Court_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
